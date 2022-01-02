7342c08119fbab762583d8f0b57767543f9e965d
Leslie Werner <lesliewerner@Leslies-MBP.lan>
2022-01-01 20:29:29 -0500

    edited

--- PyPoll.py
+++ PyPoll.py
@@ -26,4 +26,7 @@ with open(file_to_load) as election_data:
     file_reader = csv.reader(election_data)
     # Read and print the header row.
     headers = next(file_reader)
-    print(headers)
+   
+   # Print each row in the CSV file.
+    for row in file_reader:
+        print(row)
